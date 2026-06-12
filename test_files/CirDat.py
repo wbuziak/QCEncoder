24853c9d7267dcba9ae191f9e054a70fd64c999e
Xander Lewis <xander_lewis@mines.edu>
2026-06-12 10:02:39 -0600

    Implemented Will's desired target representation - Xander

--- test_files/CirDat.py
+++ test_files/CirDat.py
@@ -139,6 +139,63 @@ class cir:
             for mz in measurement:
                 f.write(f"\t\"{mz}\" [style=filled, fillcolor=yellow]\n")
             f.write("}\n")
+            
+    def graphviz_will_out(self):
+        edges = set()
+        with open(self.name + "_graphviz_will_output.txt", "w+") as f:
+            f.write("digraph G {\n")
+            for qubit in range(0, self.qCount):
+                curr = f"q{qubit}_end"
+                while curr != None:
+                    pred = self.ir[curr]
+                    if pred == None:
+                        break
+                    if "," in pred:
+                        pred = pred.split(",")
+                        for p in pred:
+                            if "end" in curr:
+                                edges.add(f"\t\"{p}\" -> \"End\"\n")
+                            elif "start" in p:
+                                edges.add(f"\t\"Start\" -> \"{curr}\"\n")
+                            else:
+                                edges.add(f"\t\"{p}\" -> \"{curr}\"\n")
+                        idx1 = pred[0].split("_")[1:-1] if pred[0].count("_") > 1 else [pred[0].split("_")[0][1:]]
+                        idx2 = pred[1].split("_")[1:-1] if pred[1].count("_") > 1 else [pred[1].split("_")[0][1:]]
+                        if "end" in curr:
+                            if any([str(qubit) == i for i in idx1]):
+                                pred = pred[0]
+                            else:
+                                pred = pred[1]
+                        elif any([i == idx1[0] or i == idx1[-1] for i in curr.split("_")[1:-1]]):
+                            pred = pred[0]
+                        else:
+                            pred = pred[1]
+                        curr = pred
+                        continue
+                    if "end" in curr:
+                        edges.add(f"\t\"{pred}\" -> \"End\"\n")
+                    elif "start" in pred:
+                        edges.add(f"\t\"Start\" -> \"{curr}\"\n")
+                    else:
+                        edges.add(f"\t\"{pred}\" -> \"{curr}\"\n")
+                    curr = pred
+            measurement = set()
+            for k in list(self.ir):
+                if "mz" in k:
+                    measurement.add(k)
+                    for i in self.ir[k].split(","):
+                        if "end" in i:
+                            edges.add(f"\t\"End\" -> \"{k}\"\n")
+                        else:
+                            edges.add(f"\t\"{i}\" -> \"{k}\"\n")
+            for edge in edges:
+                f.write(edge)
+            f.write("\n")
+            f.write(f"\t\"End\" [style=filled, fillcolor=red]\n")
+            f.write(f"\t\"Start\" [style=filled, fillcolor=green]\n")
+            for mz in measurement:
+                f.write(f"\t\"{mz}\" [style=filled, fillcolor=yellow]\n")
+            f.write("}\n")
 
 
     def __str__(self):
--- test_files/test.py
+++ test_files/test.py
@@ -3,13 +3,8 @@ from CirDat import cir
 import TargetReps as t
 
 h = quakeparser.parse_quake_file("GHZ_quake.txt")
-print(h)
-print()
-print(h.get_gates())
-print()
 h.init_ir()
-print(h.get_ir())
-print()
 t.tuple_out(h)
+t.graphviz_will_out(h)
 
 
