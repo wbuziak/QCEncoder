9cff3e30172aac07bea6f6891b581ccae1a90ec8
Xander Lewis <xander_lewis@mines.edu>
2026-06-16 07:48:00 -0600

    Added plaintext output and updated graphviz representation

--- src/TargetReps.py
+++ src/TargetReps.py
@@ -1,4 +1,5 @@
 import CirDat
+import graphviz as gv
 
 def tuple_out(cir):
     with open(cir.name + "_tuple_output.txt", "w+") as f:
@@ -10,57 +11,101 @@ def tuple_out(cir):
             f.write(f"],{g.time})\n")
 
 def graphviz_will_out(cir):
-    edges = set()
-    with open(cir.name + "_graphviz_will_output.txt", "w+") as f:
-        f.write("digraph G {\n")
-        for qubit in range(0, cir.qCount):
-            curr = f"q{qubit}_end"
-            while curr != None:
-                pred = cir.ir[curr]
-                if pred == None:
-                    break
-                if "," in pred:
-                    pred = pred.split(",")
-                    for p in pred:
-                        if "end" in curr:
-                            edges.add(f"\t\"{p}\" -> \"End\"\n")
-                        elif "start" in p:
-                            edges.add(f"\t\"Start\" -> \"{curr}\"\n")
-                        else:
-                            edges.add(f"\t\"{p}\" -> \"{curr}\"\n")
-                    idx1 = pred[0].split("_")[1:-1] if pred[0].count("_") > 1 else [pred[0].split("_")[0][1:]]
+    dag = gv.Digraph
+    dag.node('End', 'End')
+    dag.node('Start', 'Start')
+    for qubit in range(0, cir.qCount): # go through all the qubits
+        curr = f"q{qubit}_end" # our current qubit is the ith qubit
+        while curr != None: # loop backwards until None (passing the start qubit)
+            pred = cir.ir[curr] # the predecessor to the current qubit
+            if pred == None: # break early if the predecessor is None (we have already added the edge from the current gate/qubit to the next gate)
+                break
+            if "," in pred: # if the gate has multiple inputs
+                pred = pred.split(",") # separate the inputs
+                for p in pred: # add edges for all pred -> curr pairs
                     if "end" in curr:
-                        if any([str(qubit) == i for i in idx1]):
-                            pred = pred[0]
+                        dag.node(f'{p}', f'{p}')
+                        dag.edge(f'{p}', 'End')
+                    elif "start" in p:
+                        if "mz" in curr:
+                            dag.node(f'{curr}', f'{curr}', color='yellow')
                         else:
-                            pred = pred[1]
-                    elif any([i == idx1[0] or i == idx1[-1] for i in curr.split("_")[1:-1]]):
-                        pred = pred[0]
+                            dag.node(f'{curr}', f'{curr}')
+                        dag.edge('Start', f'{curr}')
                     else:
-                        pred = pred[1]
-                    curr = pred
-                    continue
-                if "end" in curr:
-                    edges.add(f"\t\"{pred}\" -> \"End\"\n")
-                elif "start" in pred:
-                    edges.add(f"\t\"Start\" -> \"{curr}\"\n")
+                        if "mz" in curr:
+                            dag.node(f'{curr}', f'{curr}', color='yellow')
+                        else:
+                            dag.node(f'{curr}', f'{curr}')
+                        dag.edge(f'{p}', f'{curr}')
+                if len(pred) > 2: # this is a special case with the measurement as it is tied to all qubits, we just select the one we started on and take that path
+                    curr = pred[qubit] 
                 else:
-                    edges.add(f"\t\"{pred}\" -> \"{curr}\"\n")
-                curr = pred
-        measurement = set()
-        for k in list(cir.ir):
-            if "mz" in k:
-                measurement.add(k)
-                for i in cir.ir[k].split(","):
-                    if "end" in i:
-                        edges.add(f"\t\"End\" -> \"{k}\"\n")
+                    curr = pred[0] if str(qubit) in pred[0].split("_") else pred[1] # choose the matching predecessor (equivalent qubit)
+                continue
+            # if it is a single input gate, just add the edge
+            if "end" in curr:
+                dag.node(f'{pred}', f'{pred}')
+                dag.edge(f'{pred}', 'End')
+            elif "start" in pred:
+                if "mz" in curr:
+                    dag.node(f'{curr}', f'{curr}', color='yellow')
+                else:
+                    dag.node(f'{curr}', f'{curr}')
+                dag.edge('Start', f'{curr}')
+            else:
+                if "mz" in curr:
+                    dag.node(f'{curr}', f'{curr}', color='yellow')
+                else:
+                    dag.node(f'{curr}', f'{curr}')
+                dag.edge(f'{pred}', f'{curr}')
+            curr = pred # curr becomes pred
+    dag.render()
+
+def plaintext_out(cir):
+    with open(cir.name + "_plaintext_output.txt", "w+") as f:
+        for g in cir.gates:
+            f.write(f"{g}\n")
+
+def graphviz_out(cir):
+    dag = gv.Digraph
+    for qubit in range(0, cir.qCount): # go through all the qubits
+        curr = f"q{qubit}_end" # our current qubit is the ith qubit
+        while curr != None: # loop backwards until None (passing the start qubit)
+            pred = cir.ir[curr] # the predecessor to the current qubit
+            if pred == None: # break early if the predecessor is None (we have already added the edge from the current gate/qubit to the next gate)
+                break
+            if "," in pred: # if the gate has multiple inputs
+                pred = pred.split(",") # separate the inputs
+                for p in pred: # add edges for all pred -> curr pairs
+                    if "end" in curr:
+                        dag.node(f'{curr}', f'{curr}', color='red')
+                    elif "mz" in curr:
+                        dag.node(f'{curr}', f'{curr}', color='yellow')
                     else:
-                        edges.add(f"\t\"{i}\" -> \"{k}\"\n")
-        for edge in edges:
-            f.write(edge)
-        f.write("\n")
-        f.write(f"\t\"End\" [style=filled, fillcolor=red]\n")
-        f.write(f"\t\"Start\" [style=filled, fillcolor=green]\n")
-        for mz in measurement:
-            f.write(f"\t\"{mz}\" [style=filled, fillcolor=yellow]\n")
-        f.write("}\n")
+                        dag.node(f'{curr}', f'{curr}')
+                    if "start" in p:
+                        dag.node(f'{p}', f'{p}', color='green')
+                    else:
+                        dag.node(f'{p}', f'{p}')
+                    dag.edge(f'{p}', f'{curr}')
+                if len(pred) > 2: # this is a special case with the measurement as it is tied to all qubits, we just select the one we started on and take that path
+                    curr = pred[qubit] 
+                else:
+                    curr = pred[0] if str(qubit) in pred[0].split("_") else pred[1] # choose the matching predecessor (equivalent qubit)
+                continue
+            # if it is a single input gate, just add the edge
+            if "end" in curr:
+                dag.node(f'{curr}', f'{curr}', color='red')
+            elif "mz" in curr:
+                dag.node(f'{curr}', f'{curr}', color='yellow')
+            else:
+                dag.node(f'{curr}', f'{curr}')
+            if "start" in pred:
+                dag.node(f'{pred}', f'{pred}', color='green')
+            else:
+                dag.node(f'{pred}', f'{pred}')
+            dag.edge(f'{pred}', f'{curr}')
+            curr = pred # curr becomes pred
+    dag.render()
+            
