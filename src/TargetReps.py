069d222e8055f6d7aec2f93d3a1b47c01c65d9aa
Xander Lewis <xander_lewis@mines.edu>
2026-06-16 14:20:56 -0600

    Fixed error in code, added comments

--- src/TargetReps.py
+++ src/TargetReps.py
@@ -11,51 +11,50 @@ def tuple_out(cir: cir):
             f.write(f"],{g.time})\n")
 
 def graphviz_will_out(cir: cir):
-    dag = gv.Digraph(format='svg', name=f'{cir.name}_DAG_Will')
-    dag.node('Start', 'Start', style='filled', color='green')
-    dag.node('End', 'End', style='filled', color='red')
-    for g in cir.gates:
+    dag = gv.Digraph(format='svg', name=f'{cir.name}_DAG_Will') # creates digraph object
+    dag.node('Start', 'Start', style='filled', color='green') # creates start node and makes it green
+    dag.node('End', 'End', style='filled', color='red') # creates end node and makes it red
+    for g in cir.gates: # creates all gate nodes including measurements
         if "mz" in g.name:
-            dag.node(f'{g}', f"{g.name[g.name.index('.')+1:]}", style='filled', color='yellow')
+            dag.node(f'{g}', f"{g.name[g.name.index('.')+1:]}", style='filled', color='yellow') # makes measurement nodes and makes them yellow
         else:
-            dag.node(f'{g}', f"{g.name[g.name.index('.')+1:]}")
-    for g in cir.gates:   
-        for i in cir.ir[g]:
-            if type(i) == str:
-                dag.edge('Start', f'{g}')
-            else:
-                dag.edge(f'{i}', f'{g}')
-    for i in cir.ir:
+            dag.node(f'{g}', f"{g.name[g.name.index('.')+1:]}") # makes non measurement gate nodes
+    for i in cir.ir: # makes all edges
+        if len(cir.ir[i]) > 0:
+            for j in cir.ir[i]:
+                if type(i) == str:
+                    if type(j) != str:
+                        dag.edge(f'{j}', 'End') # connects end node to ending gates
+                    else:
+                        dag.edge('Start', f'{i}') # for edge case of start node connecting directly to end node
+                else:
+                    if type(j) != str:
+                        dag.edge(f'{j}', f'{i}') # connects internal nodes
+                    else:
+                        dag.edge('Start', f'{i}') # connects start nodes to starting gates
+    dag.render(directory='../output') # outputs into file
+
+def graphviz_out(cir: cir):
+    dag = gv.Digraph(format='svg', name=f'{cir.name}_DAG') # creates digraph object
+    for i in cir.ir: # creates all gate nodes including measurements and all start and end qubit nodes
         if type(i) == str:
             if 'end' in i:
-                dag.edge(f'{g}', 'End')
-    dag.render(directory='../output')
+                dag.node(f'{i}', f'{i}', style='filled', color = 'red') # makes start qubit nodes and makes them green
+            elif 'start' in i:
+                dag.node(f'{i}', f'{i}', style='filled', color = 'green') # makes end qubit nodes and makes them red
+        else:
+            if "mz" in i.name:
+                dag.node(f'{i}', f"{i.name[i.name.index('.')+1:]}", style='filled', color='yellow') # makes measurement nodes and makes them yellow
+            else:
+                dag.node(f'{i}', f"{i.name[i.name.index('.')+1:]}") # makes non measurement gate nodes
+    for i in cir.ir: # makes all edges
+        if len(cir.ir[i]) > 0:
+            for j in cir.ir[i]:
+                dag.edge(f'{j}', f'{i}')
+    dag.render(directory='../output') # outputs into file
     
-
 def plaintext_out(cir: cir):
     with open("../output/" + cir.name + "_plaintext_output.txt", "w+") as f:
         for g in cir.gates:
             f.write(f"{g}\n")
-
-def graphviz_out(cir: cir):
-    dag = gv.Digraph(format='svg', name=f'{cir.name}_DAG')
-    for i in cir.ir:
-        if type(i) == str:
-            if 'end' in i:
-                dag.node(f'{i}', f'{i}', style='filled', color = 'red')
-            elif 'start' in i:
-                dag.node(f'{i}', f'{i}', style='filled', color = 'green')
-        else:
-            if "mz" in i.name:
-                dag.node(f'{i}', f"{i.name[i.name.index('.')+1:]}", style='filled', color='yellow')
-            else:
-                dag.node(f'{i}', f"{i.name[i.name.index('.')+1:]}")
-    for g in cir.gates:   
-        for i in cir.ir[g]:
-            dag.edge(f'{i}', f'{g}')
-    for i in cir.ir:
-        if type(i) == str:
-            if 'end' in i:
-                dag.edge(f'{g}', f'{i}')
-    dag.render(directory='../output')
             
